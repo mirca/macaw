e38d36fd38da7370343cad90a132995c6d2cc9e7
Ze Vinicius <jvmirca@gmail.com>
2017-10-30 10:44:52 -0700

    add optimizers module; add simple GradientDescent class

---
+++ macaw/optimizers.py
@@ -0,0 +1,51 @@
+__all__ = ['GradientDescent']
+
+
+class GradientDescent(object):
+    """Implements a simple gradient descent algorithm to find local
+    minimum of functions.
+
+    Attributes
+    ----------
+    loss_function : callable
+        A callable that has a ``gradient`` method such that
+        ``loss_function.gradient(*args)`` return the gradient value evaluated
+        at ``args``.
+    """
+
+    def __init__(self, loss_function, learning_rate=0.07):
+        self.loss_function = loss_function
+        self.learning_rate = learning_rate
+
+    def compute(self, x0, n=100, xtol=1e-4, ftol=1e-5):
+        i = 0
+        while i < n:
+            loss_before = self.loss_function(x0)
+            x_tmp = x0
+            x0 = x0 - self.learning_rate * self.loss_function.gradient(x0)
+            loss_after = self.loss_function(x0)
+
+            if abs(loss_after - loss_before) / loss_before < ftol:
+                msg = ("Loss function has not changed by {} since the previous"
+                       " iteration".format(ftol))
+                self.save_state(x0, loss_after, i+1, msg)
+                break
+
+            if abs(x_tmp - x0 < xtol).all():
+                msg = ("Optimal parameters have not changed by {} since"
+                       "the previous iteration.".format(xtol))
+                self.save_state(x0, loss_after, i+1, msg)
+                break
+            i += 1
+
+        if i == n:
+            msg = ("Max. number of iterations ({}) reached."
+                   "The algorithm might not have converged.".format(n))
+            self.save_state(x0, loss_after, n, msg)
+
+    def save_state(self, x0, funval, n_iter, message):
+        self.x = x0
+        self.fun = funval
+        self.niters = n_iter
+        self.message = message
+
