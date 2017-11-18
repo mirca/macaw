0e1844b3c380e3e49501daa47212901b3e635f19
Ze Vinicius <jvmirca@gmail.com>
2017-11-18 14:53:48 -0800

    add L1Norm

--- macaw/objective_functions.py
+++ macaw/objective_functions.py
@@ -1,11 +1,88 @@
 import numpy as np
 from inspect import signature
 from oktopus.loss import LossFunction
-from .optimizers import GradientDescent
+from .optimizers import GradientDescent, MajorizationMinimization
 
 
-__all__ = ['L2Norm']
+__all__ = ['L1Norm', 'L2Norm']
 
+
+class L1Norm(LossFunction):
+    r"""Defines the L1 Norm loss function. L1 norm is usually useful
+    to optimize the "median" model, i.e., it is more robust to
+    outliers than the quadratic loss function.
+
+    .. math::
+
+        \arg \min_{\theta \in \Theta} \sum_k |y_k - f(x_k, \theta)|
+
+    Attributes
+    ----------
+    y : array-like
+        Observed data
+    model : callable
+        A functional form that defines the model
+    regularization : callable
+        A functional form that defines the regularization term
+
+    Examples
+    --------
+    >>> from macaw.objective_functions import L1Norm
+    >>> from macaw.optimizers import GradientDescent, MajorizationMinimization
+    >>> from oktopus.models import LineModel
+    >>> import numpy as np
+    >>> # generate fake data
+    >>> np.random.seed(0)
+    >>> x = np.linspace(0, 10, 200)
+    >>> fake_data = x * 3 + 10 + np.random.normal(scale=2, size=x.shape)
+    >>> # build the model
+    >>> my_line = LineModel(x)
+    >>> # build the objective function
+    >>> l1norm = L1Norm(fake_data, my_line)
+    >>> # perform optimization
+    >>> mm = MajorizationMinimization(l1norm)
+    >>> mm.compute(x0=(1., 1.))
+    >>> print(mm.x)
+    [  2.96298344  10.27857897]
+    """
+
+    def __init__(self, y, model):
+        self.y = y
+        self.model = model
+
+    def __repr__(self):
+        return "<L1Norm(y={}, model={})>".format(self.y, self.model)
+
+    def evaluate(self, theta):
+        return np.nansum(np.absolute(self.y - self.model(*theta)))
+
+    def surrogate_fun(self, theta, theta_n):
+        """Evaluates a surrogate function that majorizes the L1Norm."""
+        r = self.y - self.model(*theta)
+        abs_r = np.abs(self.y - self.model(*theta_n))
+        return .5 * np.nansum(r * r / abs_r + abs_r)
+
+    def gradient_surrogate(self, theta, theta_n):
+        """Computes the gradient of the surrogate function."""
+        _grad = lambda model, argnum, theta: model.gradient(*theta)[argnum]
+        n_theta = len(theta)
+        r = self.y - self.model(*theta)
+        abs_r = np.abs(self.y - self.model(*theta_n))
+        grad_norm = np.array([])
+        for i in range(n_theta):
+            grad = _grad(self.model, i, theta)
+            grad_norm = np.append(grad_norm,
+                                  - np.nansum(r * grad / abs_r)
+                                 )
+        return grad_norm
+
+    def fit(self, x0=None, n=100, xtol=1e-6, ftol=1e-6):
+        if x0 is None:
+            n_theta = len(signature(self.model.evaluate).parameters)
+            x0 = np.random.uniform(low=-1, high=1, size=n_theta)
+        mm = MajorizationMinimization(self)
+        mm.compute(x0, n, xtol, ftol)
+        return mm
 
 class L2Norm(LossFunction):
     r"""Defines the squared L2 norm loss function. L2 norm
@@ -24,6 +101,7 @@ class L2Norm(LossFunction):
 
     Examples
     --------
+    >>> import numpy as np
     >>> from macaw.objective_functions import L2Norm
     >>> from macaw.optimizers import GradientDescent
     >>> from oktopus.models import LineModel
@@ -36,8 +114,10 @@ class L2Norm(LossFunction):
     >>> # build the objective function
     >>> l2norm = L2Norm(fake_data, my_line)
     >>> # perform optimization
-    >>> gd = GradientDescent(l2norm)
+    >>> gd = GradientDescent(l2norm.evaluate, l2norm.gradient)
     >>> gd.compute(x0=(1., 1.))
+    >>> print(gd.x)
+    [  2.96264076  10.32861659]
     """
 
     def __init__(self, y, model):
@@ -47,25 +127,25 @@ class L2Norm(LossFunction):
     def __repr__(self):
         return "<L2Norm(y={}, model={})>".format(self.y, self.model)
 
-    def evaluate(self, params):
-        residual = self.y - self.model(*params)
+    def evaluate(self, theta):
+        residual = self.y - self.model(*theta)
         return .5 * np.nansum(residual * residual)
 
-    def gradient(self, params):
-        _grad = lambda model, argnum, params: model.gradient(*params)[argnum]
-        n_params = len(params)
+    def gradient(self, theta):
+        _grad = lambda model, argnum, theta: model.gradient(*theta)[argnum]
+        n_theta = len(theta)
         grad_norm = np.array([])
-        for i in range(n_params):
-            grad = _grad(self.model, i, params)
+        for i in range(n_theta):
+            grad = _grad(self.model, i, theta)
             grad_norm = np.append(grad_norm,
-                                  - np.nansum((self.y - self.model.evaluate(*params)) * grad)
+                                  - np.nansum((self.y - self.model.evaluate(*theta)) * grad)
                                  )
         return grad_norm
 
     def fit(self, x0=None, n=100, xtol=1e-6, ftol=1e-6):
         if x0 is None:
-            n_params = len(signature(self.model.evaluate).parameters)
-            x0 = np.random.uniform(low=-1, high=1, size=n_params)
-        gd = GradientDescent(self)
+            n_theta = len(signature(self.model.evaluate).parameters)
+            x0 = np.random.uniform(low=-1, high=1, size=n_theta)
+        gd = GradientDescent(self.evaluate, self.gradient)
         gd.compute(x0, n, xtol, ftol)
         return gd
