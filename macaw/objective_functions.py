a33e73de1e04b13b51dfa5bc8d59dd1285703fe9
Ze Vinicius <jvmirca@gmail.com>
2017-11-24 17:54:49 -0800

    add BernoulliLikelihood; sketch of LinearLogisticRegression

--- macaw/objective_functions.py
+++ macaw/objective_functions.py
@@ -1,24 +1,11 @@
 import sys
 import numpy as np
+from oktopus.likelihood import Likelihood
 from oktopus.loss import LossFunction
 from .optimizers import GradientDescent, MajorizationMinimization
 
 
-__all__ = ['L1Norm', 'L2Norm']
-
-
-if sys.version_info[0] == 2:
-    from inspect import getargspec
-    def _get_number_of_arguments(func):
-        list_of_args = getargspec(func).args
-        if 'self' in list_of_args:
-            return len(list_of_args) - 1
-        else:
-            return len(list_of_args)
-else:
-    from inspect import signature
-    def _get_number_of_arguments(func):
-        return len(signature(func).parameters)
+__all__ = ['L1Norm', 'L2Norm', 'BernoulliLikelihood']
 
 
 class L1Norm(LossFunction):
@@ -57,7 +44,7 @@ class L1Norm(LossFunction):
     >>> mm = MajorizationMinimization(l1norm)
     >>> mm.compute(x0=(1., 1.))
     >>> print(mm.x)
-    [  2.96298402  10.27857625]
+    [  2.96298429  10.27857368]
     """
 
     def __init__(self, y, model):
@@ -81,15 +68,13 @@ class L1Norm(LossFunction):
         r = self.y - self.model(*theta)
         abs_r = np.abs(self.y - self.model(*theta_n))
         grad_model = self.model.gradient(*theta)
-        return - np.nansum(r * grad_model / abs_r, axis=1)
+        return - np.nansum(r * grad_model / abs_r, axis=-1)
 
-    def fit(self, x0=None, n=100, xtol=1e-6, ftol=1e-6):
-        if x0 is None:
-            n_theta = _get_number_of_arguments(self.model.evaluate)
-            x0 = np.random.uniform(low=-1, high=1, size=n_theta)
+    def fit(self, x0, n=100, xtol=1e-6, ftol=1e-6):
         mm = MajorizationMinimization(self)
-        mm.compute(x0, n, xtol, ftol)
+        mm.compute(x0=x0, n=n, xtol=xtol, ftol=ftol)
         return mm
+
 
 class L2Norm(LossFunction):
     r"""Defines the squared L2 norm loss function. L2 norm
@@ -97,11 +82,11 @@ class L2Norm(LossFunction):
 
     .. math::
 
-        {\arg\,min}_{\mathbf{w} \in \mathcal{W}}  \frac{1}{2}||y - f(X, \mathbf{w})||^{2}_{2}
+        {\arg\,\min}_{\mathbf{w} \in \mathcal{W}}  \frac{1}{2}||y - f(X, \mathbf{w})||^{2}_{2}
 
     Attributes
     ----------
-    data : array-like
+    y : array-like
         Observed data
     model : callable
         A functional form that defines the model
@@ -124,7 +109,7 @@ class L2Norm(LossFunction):
     >>> gd = GradientDescent(l2norm.evaluate, l2norm.gradient)
     >>> gd.compute(x0=(1., 1.))
     >>> print(gd.x)
-    [  2.96264076  10.32861659]
+    [  2.96264043  10.32861654]
     """
 
     def __init__(self, y, model):
@@ -140,18 +125,46 @@ class L2Norm(LossFunction):
 
     def gradient(self, theta):
         grad = self.model.gradient(*theta)
-        return - np.nansum((self.y - self.model(*theta)) * grad, axis=1)
+        return - np.nansum((self.y - self.model(*theta)) * grad, axis=-1)
 
-    def fit(self, x0=None, n=100, xtol=1e-6, ftol=1e-6):
-        if x0 is None:
-            n_theta = _get_number_of_arguments(self.model.evaluate)
-            x0 = np.random.uniform(low=-1, high=1, size=n_theta)
+    def fit(self, x0, n=100, xtol=1e-6, ftol=1e-6):
         gd = GradientDescent(self.evaluate, self.gradient)
-        gd.compute(x0, n, xtol, ftol)
+        gd.compute(x0=x0, n=n, xtol=xtol, ftol=ftol)
         return gd
 
 
-class BernoulliLikelihood(LossFunction):
+class BernoulliLikelihood(Likelihood):
+    r"""Implements the negative log likelihood function for independent
+    (possibly non-identical distributed) Bernoulli random variables.
+    This class also contains a method to compute maximum likelihood estimators
+    for the probability of a success.
+
+    More precisely, the MLE is computed as
+
+    .. math::
+        {\arg\,\min}_{\bm{\theta} \in \Theta} - \sum_{i=1}^{n} y_i\log(\pi(\bm{\theta})) + (1 - y_i)\log(1 - \pi(\bm{\theta}))
+
+    Attributes
+    ----------
+    y : array-like
+        Observed data
+    model : callable
+        A functional form that defines the model for the probability of success
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from macaw import BernoulliLikelihood
+    >>> from oktopus.models import ConstantModel as constant
+    >>> # generate integer fake data in the set {0, 1}
+    >>> np.random.seed(0)
+    >>> y = np.random.choice([0, 1], size=100)
+    >>> p = constant()
+    >>> logL = BernoulliLikelihood(y=y, model=p)
+    >>> result = logL.fit(x0=[0.3])
+    >>> print(result.x)
+    [ 0.55999924]
+    """
 
     def __init__(self, y, model):
         self.y = y
@@ -165,4 +178,17 @@ class BernoulliLikelihood(LossFunction):
     def gradient(self, theta):
         model_theta = self.model(*theta)
         grad = self.model.gradient(*theta)
-        return - np.nansum((1 / (1 - model_theta)) * (self.y / (1 + model_theta) - 1) * grad, axis=1)
+        return - np.nansum(self.y * grad / model_theta
+                           - (1 - self.y) * grad / (1 - model_theta),
+                           axis=-1)
+
+    def fit(self, x0, n=100, xtol=1e-6, ftol=1e-6):
+        gd = GradientDescent(self.evaluate, self.gradient)
+        gd.compute(x0=x0, n=n, xtol=xtol, ftol=ftol)
+        return gd
+
+class LinearLogisitcRegression(BernoulliLikelihood):
+    def __init__(self, y, X):
+        self.y = y
+        self.X = X
+        self.model = LineModel(X)
