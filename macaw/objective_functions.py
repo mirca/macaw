d81934d5f5e50dd1df95d4be4663342045f689d5
Ze Vinicius <jvmirca@gmail.com>
2017-11-25 13:18:08 -0800

    add support for uncertainties

--- macaw/objective_functions.py
+++ macaw/objective_functions.py
@@ -1,14 +1,55 @@
-import sys
+from abc import abstractmethod
 import numpy as np
-from oktopus.likelihood import Likelihood
-from oktopus.loss import LossFunction
 from .optimizers import GradientDescent, MajorizationMinimization
 
 
 __all__ = ['L1Norm', 'L2Norm', 'BernoulliLikelihood']
 
 
-class L1Norm(LossFunction):
+class ObjectiveFunction(object):
+    """An abstract class for a generic objective function."""
+
+    def __call__(self, theta):
+        """Calls :func:`evaluate`"""
+        return self.evaluate(theta)
+
+    @abstractmethod
+    def evaluate(self, theta):
+        """
+        Returns the objective function evaluated at theta
+
+        Parameters
+        ----------
+        theta : ndarray
+            parameter vector of the model
+
+        Returns
+        -------
+        objective_fun : scalar
+            Returns the scalar value of the objective function evaluated at
+            **params**
+        """
+        pass
+
+    def fisher_information_matrix(self, theta):
+        n_params = len(theta)
+        fisher = np.empty(shape=(n_params, n_params))
+        grad_model = self.model.gradient(*theta)
+        model = self.model(*theta)
+
+        for i in range(n_params):
+            for j in range(i, n_params):
+                fisher[i, j] = (grad_model[i] * grad_model[j] / model).sum()
+                fisher[j, i] = fisher[i, j]
+
+        return fisher
+
+    def uncertainties(self, theta):
+        inv_fisher = np.linalg.inv(self.fisher_information_matrix(theta))
+        return np.sqrt(np.diag(inv_fisher))
+
+
+class L1Norm(ObjectiveFunction):
     r"""Defines the L1 Norm loss function. L1 norm is usually useful
     to optimize the "median" model, i.e., it is more robust to
     outliers than the quadratic loss function.
@@ -23,13 +64,11 @@ class L1Norm(LossFunction):
         Observed data
     model : callable
         A functional form that defines the model
-    regularization : callable
-        A functional form that defines the regularization term
 
     Examples
     --------
     >>> from macaw.objective_functions import L1Norm
-    >>> from macaw.optimizers import GradientDescent, MajorizationMinimization
+    >>> from macaw.optimizers import MajorizationMinimization
     >>> from oktopus.models import LineModel
     >>> import numpy as np
     >>> # generate fake data
@@ -43,8 +82,12 @@ class L1Norm(LossFunction):
     >>> # perform optimization
     >>> mm = MajorizationMinimization(l1norm)
     >>> mm.compute(x0=(1., 1.))
+    >>> # get best fit parameters
     >>> print(mm.x)
     [  2.96298429  10.27857368]
+    >>> # get uncertainties on the best fit parameters
+    >>> print(l1norm.uncertainties(mm.x))
+    [ 0.11554496  0.55774923]
     """
 
     def __init__(self, y, model):
@@ -76,7 +119,7 @@ class L1Norm(LossFunction):
         return mm
 
 
-class L2Norm(LossFunction):
+class L2Norm(ObjectiveFunction):
     r"""Defines the squared L2 norm loss function. L2 norm
     tends to fit the model to the mean trend of the data.
 
@@ -90,6 +133,8 @@ class L2Norm(LossFunction):
         Observed data
     model : callable
         A functional form that defines the model
+    yerr : scalar or array-like
+        Weights or uncertainties on each observed data point
 
     Examples
     --------
@@ -108,12 +153,17 @@ class L2Norm(LossFunction):
     >>> # perform optimization
     >>> gd = GradientDescent(l2norm.evaluate, l2norm.gradient)
     >>> gd.compute(x0=(1., 1.))
+    >>> # get the best fit parameters
     >>> print(gd.x)
     [  2.96264043  10.32861654]
+    >>> # get uncertainties on the best fit parameters
+    >>> print(l2norm.uncertainties(gd.x))
+    [ 0.11568702  0.55871659]
     """
 
-    def __init__(self, y, model):
+    def __init__(self, y, model, yerr=1):
         self.y = y
+        self.yerr = yerr
         self.model = model
 
     def __repr__(self):
@@ -121,11 +171,12 @@ class L2Norm(LossFunction):
 
     def evaluate(self, theta):
         residual = self.y - self.model(*theta)
-        return .5 * np.nansum(residual * residual)
+        return .5 * np.nansum(residual * residual / (self.yerr * self.yerr))
 
     def gradient(self, theta):
         grad = self.model.gradient(*theta)
-        return - np.nansum((self.y - self.model(*theta)) * grad, axis=-1)
+        return - np.nansum((self.y - self.model(*theta)) * grad
+                           / (self.yerr * self.yerr), axis=-1)
 
     def fit(self, x0, n=100, xtol=1e-6, ftol=1e-6):
         gd = GradientDescent(self.evaluate, self.gradient)
@@ -133,7 +184,7 @@ class L2Norm(LossFunction):
         return gd
 
 
-class BernoulliLikelihood(Likelihood):
+class BernoulliLikelihood(ObjectiveFunction):
     r"""Implements the negative log likelihood function for independent
     (possibly non-identical distributed) Bernoulli random variables.
     This class also contains a method to compute maximum likelihood estimators
@@ -159,11 +210,22 @@ class BernoulliLikelihood(Likelihood):
     >>> # generate integer fake data in the set {0, 1}
     >>> np.random.seed(0)
     >>> y = np.random.choice([0, 1], size=100)
+    >>> # create a model
     >>> p = constant()
-    >>> logL = BernoulliLikelihood(y=y, model=p)
-    >>> result = logL.fit(x0=[0.3])
+    >>> # perform optimization
+    >>> ber = BernoulliLikelihood(y=y, model=p)
+    >>> result = ber.fit(x0=[0.3])
+    >>> # get best fit parameters
     >>> print(result.x)
     [ 0.55999924]
+    >>> print(np.mean(y>0)) # theorectical MLE
+    0.56
+    >>> # get uncertainties on the best fit parameters
+    >>> print(ber.uncertainties(result.x))
+    [ 0.0496387]
+    >>> # theorectical uncertainty
+    >>> print(np.sqrt(.56 * .44 / 100))
+    0.049638694584
     """
 
     def __init__(self, y, model):
@@ -182,10 +244,15 @@ class BernoulliLikelihood(Likelihood):
                            - (1 - self.y) * grad / (1 - model_theta),
                            axis=-1)
 
+    def fisher_information_matrix(self, theta):
+        return len(self.y) * super(BernoulliLikelihood,
+               self).fisher_information_matrix(theta) / (1 - self.model(*theta))
+
     def fit(self, x0, n=100, xtol=1e-6, ftol=1e-6):
         gd = GradientDescent(self.evaluate, self.gradient)
         gd.compute(x0=x0, n=n, xtol=xtol, ftol=ftol)
         return gd
+
 
 class LinearLogisitcRegression(BernoulliLikelihood):
     def __init__(self, y, X):
