c8ac31ef734dbe605e88ed9c45e247512ea2073f
Ze Vinicius <jvmirca@gmail.com>
2017-11-25 13:18:35 -0800

    compare results against sklearn

--- macaw/tests/test_optimization.py
+++ macaw/tests/test_optimization.py
@@ -1,5 +1,6 @@
 import numpy as np
 from oktopus.models import LineModel
+from sklearn import datasets, linear_model
 from numpy.testing import assert_allclose
 from ..objective_functions import L1Norm, L2Norm
 from ..optimizers import GradientDescent, MajorizationMinimization
@@ -22,3 +23,24 @@ def test_fitting_line():
     l1norm = L1Norm(fake_data, my_line)
     mm = MajorizationMinimization(l1norm)
     mm.compute(x0=(1., 1.))
+    assert_allclose(mm.x, [3., 10.], rtol=1e-1)
+
+def test_ordinary_least_squares_against_sklearn():
+    # Load the diabetes dataset
+    diabetes = datasets.load_diabetes()
+    # Use only one feature
+    diabetes_X = diabetes.data[:, np.newaxis, 2]
+    # Get training set
+    diabetes_X_train = diabetes_X[:-20]
+    diabetes_y_train = diabetes.target[:-20]
+    # Create and train linear regression object
+    regr = linear_model.LinearRegression()
+    regr.fit(diabetes_X_train, diabetes_y_train)
+
+    # Using macaw
+    model_train = LineModel(diabetes_X_train.reshape(-1))
+    l2norm = L2Norm(y=diabetes_y_train.reshape(-1), model=model_train)
+    results = l2norm.fit(x0=[0., 0.])
+
+    assert_allclose(regr.coef_, results.x[0])
+    assert_allclose(regr.intercept_, results.x[1])
